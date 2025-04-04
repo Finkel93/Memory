5378755e7e183a71b150b78bf7e3c0bb1c299d0e
Philipp Lode <philipp.lode@htwg-konstanz.de>
2025-04-04 09:32:39 +0200

    renamed a function

--- memory/src/main/scala/Main.scala
+++ memory/src/main/scala/Main.scala
@@ -4,16 +4,16 @@ def main(): Unit = {
   val height = 26    // 4x + 2
   val width = 72     // 7x + 2
 
-  spielerErstellen(1, 4)
+  displayPlayer(1, 4)
 
   feldErstellen(height, width)
 
-  spielerErstellen(2, 4)
+  displayPlayer(2, 4)
 
 }
 
 
-def spielerErstellen (id:Int, karten:Int) : Unit = {
+def displayPlayer (id:Int, karten:Int) : Unit = {
 
   val top = "Spieler " + id + ":\n ----- \n"
   val mid = "|     |\n" * 2
