e098d5e8907e3a2a724b35b72d6b62129e5573d0
VincentR3 <vincent03@gmx.de>
2025-07-03 17:38:29 +0200

    C1

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM hseeberger/scala-sbt:17.0.2_1.6.2_3.1.1
-
-RUN apt-get update && apt-get install -y libxrender1 libxtst6 libxi6 libxext6
-
-WORKDIR /app
-
-COPY . .
-
-CMD ["sbt", "run"]
--- src/main/scala/de/htwg/se/memory/controller/state/GameState.scala
+++ src/main/scala/de/htwg/se/memory/controller/state/GameState.scala
@@ -3,7 +3,7 @@ package de.htwg.se.memory.controller.state
 import de.htwg.se.memory.controller.Controller
 import scala.util.{Try, Failure}
 
-// Gemeinsames Interface für alle Spielzustände
+
 trait GameState {
   def handleInput(input: Int, controller: Controller): Unit
   def name: String
